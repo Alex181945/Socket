1c4a08c07d7c92f991f7f014cec5da0e2da45873
alest <alest@Alejandro>
2015-10-16 23:48:57 -0500

    Solo se corrige identacion

--- Socket/src/com/itgam/cachorros/Cliente.java
+++ Socket/src/com/itgam/cachorros/Cliente.java
@@ -5,51 +5,51 @@ import java.net.*;
 
 public class Cliente {
 	
-public static void main(String[] args){
-        
-        InetAddress direccion = null;
-        
-        try {
-            direccion = InetAddress.getLocalHost();
-        } catch (UnknownHostException e) {
-            System.out.println("Hubo un error al obtenet la direccion, "+e);
-            System.err.println(-1);
-        }
-        
-        int puerto = 1234;
-        
-        for (int i = 1; i < args.length; i++) {
-            Socket sckt = null;
-            DataInputStream dis = null;
-            DataOutputStream dos = null;
-            
-            try {
-                int numero = 10;
-                
-                sckt = new Socket(direccion,puerto);
-                
-                dis = new DataInputStream(sckt.getInputStream());
-                dos = new DataOutputStream(sckt.getOutputStream());
-                
-                dos.writeInt(numero);
-                long resultado = dis.readLong();
-                
-                System.out.println("Solicitud = "+numero+"\tResultado = "+
-                        resultado);
-                
-                dis.close();
-                dos.close();
-            } catch (Exception e) {
-                System.err.println("Error al abrir el socket de servidor: "+e);
-            }
-            
-            try {
-                if (sckt != null){
-                    sckt.close();
-                }
-            } catch (Exception e) {
-                System.err.println("Error al cerrar el socket: "+e);
-            }
-        }
-    }
+	public static void main(String[] args){
+
+		InetAddress direccion = null;
+
+		try {
+			direccion = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			System.out.println("Hubo un error al obtenet la direccion, "+e);
+			System.err.println(-1);
+		}
+
+		int puerto = 1234;
+
+		for (int i = 1; i < args.length; i++) {
+			Socket sckt = null;
+			DataInputStream dis = null;
+			DataOutputStream dos = null;
+
+			try {
+				int numero = 10;
+
+				sckt = new Socket(direccion,puerto);
+
+				dis = new DataInputStream(sckt.getInputStream());
+				dos = new DataOutputStream(sckt.getOutputStream());
+
+				dos.writeInt(numero);
+				long resultado = dis.readLong();
+
+				System.out.println("Solicitud = "+numero+"\tResultado = "+
+						resultado);
+
+				dis.close();
+				dos.close();
+			} catch (Exception e) {
+				System.err.println("Error al abrir el socket de servidor: "+e);
+			}
+
+			try {
+				if (sckt != null){
+					sckt.close();
+				}
+			} catch (Exception e) {
+				System.err.println("Error al cerrar el socket: "+e);
+			}
+		}
+	}
 }
--- Socket/src/com/itgam/cachorros/Servidor.java
+++ Socket/src/com/itgam/cachorros/Servidor.java
@@ -7,55 +7,55 @@ public class Servidor {
 
 	public static void main(String args[]){
 		/**/
-        /*Obteniendo la direccion ip del equipo
+		/*Obteniendo la direccion ip del equipo
         Note: La direccion sin necesidad del string*/
-        try {
-            System.out.println("La direccion IP = "+InetAddress.getLocalHost().toString());
-        } catch (UnknownHostException e) {
-            System.out.println("Hubo un error al obtenet la direccion, "+e);
-        }
-        
-           /*Abriendo un puerto*/
-        ServerSocket ss = null;
-        
-        try {
-            ss = new ServerSocket(1234);
-        } catch (IOException e) {
-            System.out.println("Error al abrir el socket de servidor: "+e);
-            System.exit(-1);
-        }
-        
-        /*Variables*/
-        int entrada;
-        long salida;
-        
-        while (true) {            
-            try {
-                /*El programa se va a quedar en este paso hasta detectar una
+		try {
+			System.out.println("La direccion IP = "+InetAddress.getLocalHost().toString());
+		} catch (UnknownHostException e) {
+			System.out.println("Hubo un error al obtenet la direccion, "+e);
+		}
+
+		/*Abriendo un puerto*/
+		ServerSocket ss = null;
+
+		try {
+			ss = new ServerSocket(1234);
+		} catch (IOException e) {
+			System.out.println("Error al abrir el socket de servidor: "+e);
+			System.exit(-1);
+		}
+
+		/*Variables*/
+		int entrada;
+		long salida;
+
+		while (true) {            
+			try {
+				/*El programa se va a quedar en este paso hasta detectar una
                 conexion*/           
-                Socket sckt = ss.accept();
-                DataInputStream dis = new DataInputStream(sckt.getInputStream());
-                DataOutputStream dos = new DataOutputStream(sckt.getOutputStream());
-                
-                int puerto = sckt.getPort();
-                
-                InetAddress direccion = sckt.getInetAddress();
+				Socket sckt = ss.accept();
+				DataInputStream dis = new DataInputStream(sckt.getInputStream());
+				DataOutputStream dos = new DataOutputStream(sckt.getOutputStream());
 
-                entrada = dis.readInt();
+				int puerto = sckt.getPort();
 
-                salida = (long)entrada * (long)entrada;
+				InetAddress direccion = sckt.getInetAddress();
 
-                dos.writeLong(salida);
+				entrada = dis.readInt();
 
-                dis.close();
-                dos.close();
-                sckt.close();
+				salida = (long)entrada * (long)entrada;
 
-                System.out.println("Cliente = "+direccion+" puerto = "+puerto+
-                	"\tEntrada = "+entrada+" Salida ="+salida);
-            } catch (Exception e) {
-            	System.err.println("Se ha producido un error: "+e);
-            }
-        }
+				dos.writeLong(salida);
+
+				dis.close();
+				dos.close();
+				sckt.close();
+
+				System.out.println("Cliente = "+direccion+" puerto = "+puerto+
+						"\tEntrada = "+entrada+" Salida ="+salida);
+			} catch (Exception e) {
+				System.err.println("Se ha producido un error: "+e);
+			}
+		}
 	}
 }
